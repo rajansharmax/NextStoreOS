438da79b6a553f17a9f1a74e267e2da3f65a2964
rajansharmax <rajansharmaa46@gmail.com>
2024-02-04 01:02:45 +0530

    feat(vercel analytics): added vercel analytics

---
+++ src/app/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>Powered by Vercel</p>
+    </div>
+  );
+}
+
+export default Footer;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from '@vercel/analytics/react';
 import "./globals.css";
 import StoreProvider from "./StoreProvider";
 
@@ -22,6 +23,7 @@ export default function RootLayout({
         <body className={inter.className}>
           {children}
           <SpeedInsights />
+          <Analytics />
         </body>
       </html>
     </StoreProvider>
