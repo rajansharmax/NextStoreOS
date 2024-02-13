cee58bf7ea690e7d260581e8c7a2b16ab48d9f81
rajansharmax <rajansharmaa46@gmail.com>
2024-02-13 13:34:02 +0530

    feat(clarity): added microsoft clarity

--- src/app/components/Layout/Partials/Head/index.tsx
+++ src/app/components/Layout/Partials/Head/index.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 const Head = () => {
   return (
     <head>
@@ -26,6 +27,26 @@ const Head = () => {
       <meta property="og:title" content="Link preview title"></meta>
       <meta property="og:description" content="Link preview description" />
       <meta property="og:url" content="Canonical link preview URL"></meta>
+
+      <Script strategy="lazyOnload" id="clarity-script">
+        {`
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "l0zm17bop6");
+        `}
+      </Script>
+
+      {/* <!-- Google tag (gtag.js) --> */}
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-32F9YGZ01B"></Script>
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-32F9YGZ01B');`}
+      </Script>
     </head>
   );
 }
