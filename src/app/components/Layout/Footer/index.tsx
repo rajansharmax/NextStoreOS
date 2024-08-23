667724797644ef16653723cfd122c253c702b371
rajansharmax <rajansharmaa46@gmail.com>
2024-08-23 22:43:01 +0530

    fix(package json): updated packages and working on footer section

--- src/app/components/Layout/Footer/index.tsx
+++ src/app/components/Layout/Footer/index.tsx
@@ -1,11 +1,104 @@
 import FooterBottom from "./Footerbottom";
-import { Layout, Flex } from "antd";
+import { Layout, Flex, Col, message } from "antd";
+import { FooterWrapper, StyledList, StyledRow } from "./styled";
+import { Avatar, List, Button, Input, Form, Space } from "antd";
 const { Header, Footer, Sider, Content } = Layout;
 
 const index = () => {
+
+  const data = [
+    {
+      title: "Ant Design Title 1",
+    },
+    {
+      title: "Ant Design Title 2",
+    },
+    {
+      title: "Ant Design Title 3",
+    },
+    {
+      title: "Ant Design Title 4",
+    },
+  ];
+
+  const onFinish = (values: any) => {
+      message.success('Thanks for subscribing', 5);
+  };
+
   return (
     <>
-
+      <FooterWrapper>
+        <StyledRow>
+          <Col xs={12} sm={12} md={8} lg={6} xl={6}>
+            <h4>Subscribe to our newsletter</h4>
+            <p>Get all the latest information on events, sales and offers.</p>
+            <Form layout="inline" onFinish={onFinish}>
+              <Form.Item
+                name="email"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please enter your email!",
+                  },
+                  {
+                    type: "email",
+                    message: "Please enter a valid email!",
+                  },
+                ]}
+              >
+                <Input type="text" placeholder="Enter your email" />
+              </Form.Item>
+              <Form.Item>
+                <Button type="primary" htmlType="submit">
+                  Submit
+                </Button>
+              </Form.Item>
+            </Form>
+          </Col>
+          <Col xs={12} sm={12} md={6} lg={4} xl={4}>
+            <h4>About</h4>
+            <StyledList
+              itemLayout="horizontal"
+              dataSource={data}
+              renderItem={(item: any, index: number) => (
+                <List.Item key={index}>
+                  <List.Item.Meta
+                    title={<a href="https://ant.design">{item.title}</a>}
+                  />
+                </List.Item>
+              )}
+            />
+          </Col>
+          <Col xs={12} sm={12} md={6} lg={4} xl={4}>
+            <h4>About</h4>
+            <StyledList
+              itemLayout="horizontal"
+              dataSource={data}
+              renderItem={(item: any, index: number) => (
+                <List.Item key={index}>
+                  <List.Item.Meta
+                    title={<a href="https://ant.design">{item.title}</a>}
+                  />
+                </List.Item>
+              )}
+            />
+          </Col>
+          <Col xs={12} sm={12} md={6} lg={4} xl={4}>
+            <h4>Contact Us</h4>
+            <StyledList
+              itemLayout="horizontal"
+              dataSource={data}
+              renderItem={(item: any, index: number) => (
+                <List.Item key={index}>
+                  <List.Item.Meta
+                    title={<a href="https://ant.design">{item.title}</a>}
+                  />
+                </List.Item>
+              )}
+            />
+          </Col>
+        </StyledRow>
+      </FooterWrapper>
       <FooterBottom />
     </>
   );
--- src/app/components/Layout/Footer/styled.tsx
+++ src/app/components/Layout/Footer/styled.tsx
@@ -1,1 +1,33 @@
 import styled from "styled-components";
+import { Layout, Flex, Row } from "antd";
+import { Avatar, List } from "antd";
+const { Footer } = Layout;
+
+export const FooterWrapper = styled(Footer)`
+  background-color: rgb(13, 13, 13);
+  h4 {
+    color: #fff;
+  }
+`;
+
+export const StyledRow = styled(Row)``;
+
+export const StyledList = styled(List)`
+  .ant-list-item {
+    border: 0;
+  }
+  .ant-list-item-meta-avatar {
+    margin: 0;
+    margin-inline-end: 4px !important;
+    .ant-avatar {
+      width: 25px;
+      height: 25px;
+    }
+  }
+  .ant-list-item-meta-title > a {
+    color: #fff !important;
+    &:hover {
+      color: #00ffa3 !important;
+    }
+  }
+`;
