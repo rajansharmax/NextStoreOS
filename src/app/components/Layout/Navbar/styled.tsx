a783df8c0b0e2279f196d6373f3166e365f6542f
rajansharmax <rajansharmaa46@gmail.com>
2024-08-24 14:10:25 +0530

    fix(antd:theme config): added antd thene config

--- src/app/components/Layout/Navbar/styled.tsx
+++ src/app/components/Layout/Navbar/styled.tsx
@@ -3,9 +3,7 @@ import { Drawer } from "antd";
 
 
 export const StyledDrawer = styled(Drawer)`
-  .ant-drawer-content {
-    background-color: #171717;
-  }
+
   .ant-drawer-header-title {
     flex-direction: row-reverse;
   }
