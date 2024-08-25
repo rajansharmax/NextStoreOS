8c4e1868a0f859eb877366e53c6a19691d2ffb03
Dark <sudeep1098@gmail.com>
2024-08-26 00:11:39 +0530

    added cart dropdown

--- src/app/components/Layout/Header/MenuItems.tsx
+++ src/app/components/Layout/Header/MenuItems.tsx
@@ -6,5 +6,4 @@ export const menuItems = [
   { label: 'Order Samples', href: routes.orderSample, icon: <BulbOutlined style={{ fontSize: '20px', marginRight: "5px" }} /> },
   { label: 'My Account', href: routes.myAccount, icon: <UserOutlined style={{ fontSize: '20px', marginRight: "5px" }} /> },
   { label: 'Repeat Orders', href: routes.repeatOrders, icon: <ClockCircleOutlined style={{ fontSize: '20px', marginRight: "5px" }} /> },
-  { label: 'Cart', href: routes.cart, icon: <ShoppingCartOutlined style={{ fontSize: '24px', marginRight: "5px" }} /> }
 ];
