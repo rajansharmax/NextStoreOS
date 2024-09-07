9c2534104350d768d9c74204a4d7345ba07a2cff
rajansharmax <rajansharmaa46@gmail.com>
2024-09-07 17:49:56 +0530

    fix(base): fixing base and added next env

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+    typedRoutes: true,
+    ignoreBuildErrors: true,
+};
+
+export default nextConfig;
